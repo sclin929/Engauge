545fef360a286a952d585ff2dc3ced2c08a7a9f6
MaiHo <maingocho94@gmail.com>
2014-11-15 03:33:21 -0800

    login stuff

--- app/build/generated/source/r/debug/com/app/engauge/R.java
+++ app/build/generated/source/r/debug/com/app/engauge/R.java
@@ -15,10 +15,12 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_action_new=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
+        public static final int action_search=0x7f080001;
+        public static final int action_settings=0x7f080002;
         public static final int pager=0x7f080000;
     }
     public static final class layout {
@@ -28,24 +30,26 @@ public final class R {
         public static final int fragment_homepage_recent=0x7f030003;
     }
     public static final class menu {
-        public static final int my=0x7f070000;
+        public static final int menu_homepage_actions=0x7f070000;
+        public static final int my=0x7f070001;
     }
     public static final class string {
-        public static final int action_settings=0x7f050000;
-        public static final int add_question_activity_label=0x7f050001;
-        public static final int app_name=0x7f050002;
-        public static final int favorites=0x7f050003;
-        public static final int hello_world=0x7f050004;
-        public static final int homepage_activity_label=0x7f050005;
-        public static final int login=0x7f050006;
-        public static final int login_activity_label=0x7f050007;
-        public static final int or_spaces=0x7f050008;
-        public static final int password=0x7f050009;
-        public static final int recent=0x7f05000a;
-        public static final int sign_up=0x7f05000b;
-        public static final int signup_activity_label=0x7f05000c;
-        public static final int username=0x7f05000d;
-        public static final int view_group_activity_label=0x7f05000e;
+        public static final int action_add_group=0x7f050000;
+        public static final int action_settings=0x7f050001;
+        public static final int add_question_activity_label=0x7f050002;
+        public static final int app_name=0x7f050003;
+        public static final int favorites=0x7f050004;
+        public static final int hello_world=0x7f050005;
+        public static final int homepage_activity_label=0x7f050006;
+        public static final int login=0x7f050007;
+        public static final int login_activity_label=0x7f050008;
+        public static final int or_spaces=0x7f050009;
+        public static final int password=0x7f05000a;
+        public static final int recent=0x7f05000b;
+        public static final int sign_up=0x7f05000c;
+        public static final int signup_activity_label=0x7f05000d;
+        public static final int username=0x7f05000e;
+        public static final int view_group_activity_label=0x7f05000f;
     }
     public static final class style {
         /**  Customize your theme here. 
