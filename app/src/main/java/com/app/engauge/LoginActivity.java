a1ec364520d3f977499331f2c70a91a7d9a82fa9
Maury Quijada <maury.quijada@gmail.com>
2014-11-14 23:24:01 -0800

    Create Activities.

--- app/src/main/java/com/app/engauge/LoginActivity.java
+++ app/src/main/java/com/app/engauge/LoginActivity.java
@@ -14,7 +14,7 @@ import android.os.Build;
 import com.firebase.client.Firebase;
 
 
-public class MyActivity extends Activity {
+public class LoginActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/app/engauge/ViewGroupActivity.java
@@ -0,0 +1,7 @@
+package com.app.engauge;
+
+/**
+ * Created by Maury on 11/14/14.
+ */
+public class ViewGroupActivity {
+}
